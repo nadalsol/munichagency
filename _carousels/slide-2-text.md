e1fe9da5692111bbcb73f92877a5ab8bed938abe
Nadal Soler <nadalsoler@gmail.com>
2018-08-17 14:47:53 +0200

    feat: retrieved carousel data from DatoCMS

---
+++ _carousels/slide-2-text.md
@@ -0,0 +1,7 @@
+---
+title: Slide 2 text
+subtitle: Slide 2 text secondary
+link: "#"
+img: https://www.datocms-assets.com/5249/1534506857-bg-carousel-2.jpg
+---
+
--- dato.config.rb
+++ dato.config.rb
@@ -1,12 +1,25 @@
 # dato.config.rb
-directory "_servicios" do
-  dato.servicios.each do |item|
-    create_post "#{item.title.parameterize}.md" do
+directory "_services" do
+  dato.services.each do |service|
+    create_post "#{service.title.parameterize}.md" do
       frontmatter :yaml, {
         layout: "default",
-        title: item.title
+        title: service.title
       }
-      content item.description
+      content service.description
+    end
+  end
+end
+
+directory "_carousels" do
+  dato.carousels.each do |carousel|
+    create_post "#{carousel.title.parameterize}.md" do
+      frontmatter :yaml, {
+        title: carousel.title,
+        subtitle: carousel.subtitle,
+        link: carousel.link,
+        img: carousel.img.url
+      }
     end
   end
 end
